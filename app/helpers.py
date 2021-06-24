e8730951b16a1829a0ef4a7953ebf9a92f8e6c84
AlexBetita <alexbheb25@gmail.com>
2021-06-24 16:43:54 -0700

    new branch

---
+++ app/helpers.py
@@ -0,0 +1,36 @@
+import boto3, botocore
+import os
+from .config import Config
+
+
+
+s3 = boto3.client(
+   "s3",
+   aws_access_key_id=Config.S3_KEY,
+   aws_secret_access_key=Config.S3_SECRET_ACCESS_KEY
+)
+
+print('Config.S3_KEY,', Config.S3_KEY)
+print('Config.S3_SECRET_ACCESS_KEY,', Config.S3_SECRET_ACCESS_KEY)
+
+def upload_file_to_s3(file, bucket_name, user_info, acl="public-read"):
+
+    new_file_name = user_info + '_' + file.filename
+    try:
+
+        s3.upload_fileobj(
+            file,
+            bucket_name,
+            new_file_name,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+
+    except Exception as e:
+        # This is a catch all exception, edit this part to fit your needs.
+        print("Something Happened: ", e)
+        return e
+
+    return "{}{}".format(Config.S3_LOCATION, new_file_name)
