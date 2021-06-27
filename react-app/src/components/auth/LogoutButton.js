a3a1026ff307cfacc8433d21680d8dadc8deecb2
AlexBetita <alexbheb25@gmail.com>
2021-06-27 00:51:02 -0700

    fixed bug

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom"
 import { logout } from "../../store/session";
-import { useHistory } from "react-router-dom";
 
 const LogoutButton = () => {
   const history = useHistory();
