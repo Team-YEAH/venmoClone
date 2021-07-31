997ddaafddc7c7ca61ae1f62f131c91cc43adeb2
Jimmy Ly <lyjimmy19@gmail.com>
2021-07-30 20:57:37 -0400

    tried styling payment details page

--- react-app/src/components/PaymentOption/PaymentOption.js
+++ react-app/src/components/PaymentOption/PaymentOption.js
@@ -16,26 +16,28 @@ const PaymentOption = ({user, paymentdetails}) => {
 
     return (
         <>
-            {
-            !deleteTrigger && <tbody className='div__payment__container'>
-                <tr>
-                    <td className='payment__td'>
-                        Card #: {paymentdetails.debit_card}
-                    </td>
-                    <td className='payment__td'>
-                        Bank #: {paymentdetails.bank_number}
-                    </td>
-                    <td className='payment__td'>
-                        Bank: {paymentdetails.bank}
-                    </td>
-                    <td className='payment__td'>
-                        Billing: {paymentdetails.billing_address}
-                    </td>
-                    <button className='payment__deleteBtn' onClick={removePayment}>
-                        Delete
-                    </button>
-                </tr>
-            </tbody > }
+            <div className="paymentTable-container">
+                {
+                !deleteTrigger && <tbody className='div__payment__container'>
+                    <tr>
+                        <td className='payment__td'>
+                            Card #: {paymentdetails.debit_card}
+                        </td>
+                        <td className='payment__td'>
+                            Bank #: {paymentdetails.bank_number}
+                        </td>
+                        <td className='payment__td'>
+                            Bank: {paymentdetails.bank}
+                        </td>
+                        <td className='payment__td'>
+                            Billing: {paymentdetails.billing_address}
+                        </td>
+                        <button className='payment__deleteBtn' onClick={removePayment}>
+                            Delete
+                        </button>
+                    </tr>
+                </tbody > }
+            </div>
         </>
     )
 }
