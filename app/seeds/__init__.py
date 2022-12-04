e84866dc06ad640cec58f3971b4c75beaa96476c
Justin <just2rules@gmail.com>
2022-12-03 18:21:59 -0800

    debugged seeder

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,17 +1,28 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from app.models.db import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
 
 # Creates the `flask seed all` command
+
+
 @seed_commands.command('all')
 def seed():
+    if environment == 'production':
+        # Before seeding, truncate all tables prefixed with schema name
+        db.session.execute(
+            f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        # Add a truncate command here for every table that will be seeded.
+        db.session.commit()
     seed_users()
     # Add other seed functions here
 
 # Creates the `flask seed undo` command
+
+
 @seed_commands.command('undo')
 def undo():
     undo_users()
